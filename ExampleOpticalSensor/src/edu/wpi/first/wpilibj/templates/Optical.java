469405c7f1801ad15daef372f46820aa5b26e5e3
Peter Mitrano <mitranopeter@gmail.com>
2013-12-15 14:24:27 -0500

    optical sensor

--- ExampleOpticalSensor/src/edu/wpi/first/wpilibj/templates/Optical.java
+++ ExampleOpticalSensor/src/edu/wpi/first/wpilibj/templates/Optical.java
@@ -7,11 +7,8 @@
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,26 +17,15 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class Spike extends IterativeRobot {
+public class Optical extends IterativeRobot {
+
+    DigitalInput opticalSensor = new DigitalInput(4);
 
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    Victor motor = new Victor(1);
-    Encoder enc = new Encoder(1, 2);
-    Joystick joystick = new Joystick(1);
-    JoystickButton controlByPosition = new JoystickButton(joystick, 1);
-    DigitalInput leftLimit = new DigitalInput(1);
-    DigitalInput rightLimit = new DigitalInput(2);
-    double kP = 0.1, tolerence = 0.1;
-
     public void robotInit() {
-        // you always have to start and reset encoders
-        enc.start();
-        enc.reset();
-        // just for safety
-        motor.set(0);
 
     }
 
@@ -54,27 +40,14 @@ public class Spike extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        if (controlByPosition.get()) {
-            setToEncoderValue(joystick.getY());
-        } else {
-            motor.set(joystick.getY());
-        }
+        SmartDashboard.putBoolean("opticalSensor.get()", opticalSensor.get());
     }
 
-    public void setToEncoderValue(double pos) {
-        double error = pos - enc.getRaw();
-        if (error > tolerence) {
-            if (error * kP < 1) {
-                motor.set(error * kP);
-            } else {
-                motor.set(1);
-            }
-        } else if (error < -tolerence) {
-            if (error * kP > -1) {
-                motor.set(error * kP);
-            } else {
-                motor.set(-1);
-            }
-        }
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+
     }
+
 }
