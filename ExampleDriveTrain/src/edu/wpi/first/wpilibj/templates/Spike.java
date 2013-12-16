f587a7ec3c4ea06b9b217a9e89e5d8cbad756197
Peter Mitrano <mitranopeter@gmail.com>
2013-12-15 21:55:36 -0500

    readd back the projects

---
+++ ExampleDriveTrain/src/edu/wpi/first/wpilibj/templates/Spike.java
@@ -0,0 +1,62 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.buttons.SpikeButton;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Spike extends IterativeRobot {
+
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    Joystick leftJoystick = new Joystick(1);
+    Joystick rightJoystick = new Joystick(2);
+    Jaguar leftDrive = new Jaguar(1);
+    Jaguar rightDrive = new Jaguar(2);
+    RobotDrive drive = new RobotDrive(leftDrive,
+            rightDrive);
+    SpikeButton squaredDrive = new SpikeButton(leftJoystick, 3);
+
+    public void robotInit() {
+
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        SmartDashboard.putBoolean("squared drive: ", squaredDrive.getState());
+        if (squaredDrive.getState()) {
+            //the last parameter is to say "square the joystick values"
+            //as usual, look inthe wpilibj source if you want to see how that works
+            drive.tankDrive(leftJoystick, rightJoystick, true);
+        } else {
+            drive.tankDrive(leftJoystick.getY(), rightJoystick.getY());
+        }
+        SmartDashboard.putNumber("leftJoy: ", leftJoystick.getY());
+        SmartDashboard.putNumber("rightJoy: ", rightJoystick.getY());
+    }
+}
---
+++ ExamplePlunger/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -0,0 +1,89 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.DriverStationLCD;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class RobotTemplate extends IterativeRobot {
+
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    Jaguar motor = new Jaguar(3);
+    DigitalInput limit = new DigitalInput(4);
+    Joystick joystick = new Joystick(1);
+    DriverStationLCD LCD = DriverStationLCD.getInstance();
+    JoystickButton trigger = new JoystickButton(joystick, 1);
+    boolean movedOffLimitSwitch = false, plunge = false;
+
+    public void robotInit() {
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        if (Math.abs(joystick.getY()) > 0.05) {
+            motor.set(-joystick.getY());
+        } else {
+            if (trigger.get()) {
+                plunge = true;
+            }
+            if (plunge) {
+                if (!movedOffLimitSwitch) {
+                    if (limit.get()) {
+                        motor.set(-1);
+                    } else {
+                        movedOffLimitSwitch = true;
+                    }
+                } else {
+                    if (!limit.get()) {
+                        motor.set(-1);
+                    } else {
+                        motor.set(0.0);
+                        movedOffLimitSwitch = false;
+                        plunge = false;
+                    }
+                }
+            }
+        }
+
+        LCD.println(DriverStationLCD.Line.kUser1,
+                1, "lim value" + limit.get());
+        LCD.println(DriverStationLCD.Line.kUser2,
+                1, "Joystick Y value: " + joystick.getY());
+        LCD.println(DriverStationLCD.Line.kUser3,
+                1, "lim value" + movedOffLimitSwitch);
+        LCD.updateLCD();
+    }
+
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    }
+}
